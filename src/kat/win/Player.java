32e2de9b5317318295c860278c2018845fa5bbee
katwin <efekma@gmail.com>
2019-03-19 13:52:05 +0300

    все работает, кроме show. не прописан javadoc и команда help, а так же автосохранение при удалении элементов из коллекции.

---
+++ src/kat/win/Player.java
@@ -0,0 +1,31 @@
+package kat.win;
+
+public class Player {
+    private int size;
+    private int weight;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
